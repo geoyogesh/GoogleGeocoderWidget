39e7f387f0d258e6963bd2dca606cf6e603f80bc
geoyogesh <geoyogesh@gmail.com>
2015-06-10 17:42:12 -0400

    basic working implementation

--- Widget.js
+++ Widget.js
@@ -4,10 +4,10 @@ define(['dojo/_base/declare',
         'jimu/BaseWidget',
         'dojo/request',
        'dojo/text!./_templates/google-result.html',
-       'dojo/parser',
-       'dojo/dom-construct','dojox/dtl/_base', 'dojox/dtl/Context'
+       'dojo/parser','esri/geometry/Point','esri/SpatialReference',
+       'dojo/dom-construct', 'dojox/dtl/_base', 'dojox/dtl/Context'
        ],
-    function (declare, _AttachMixin, _WidgetsInTemplateMixin, BaseWidget, request, template,parser,domConst,dtl, context) {
+    function (declare, _AttachMixin, _WidgetsInTemplateMixin, BaseWidget, request, template, parser, Point,SpatialReference, domConst, dtl, context) {
         //To create a widget, you need to derive from BaseWidget.
         return declare([BaseWidget, _WidgetsInTemplateMixin], {
 
@@ -16,8 +16,6 @@ define(['dojo/_base/declare',
             baseClass: 'google-geocoder-widget',
             // this property is set by the framework when widget is loaded.
             // name: 'GoogleGeocoderWidget',
-            _apiUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
-            _apiKey: 'AIzaSyC33aCsvTd4vAHA2f2gigNoEkd0IsbUhzc',
             _templateString: template,
             //methods to communication with app container:
             postCreate: function () {
@@ -61,25 +59,43 @@ define(['dojo/_base/declare',
             // resize: function(){
             //   console.log('GoogleGeocoderWidget::resize');
             // }
+            zoomTo: function (x, y) {
+                console.log(x + ',' + y);
+                var point = new Point([x,y],new SpatialReference({ wkid:4326 }));
+                this.map.centerAndZoom(point,12);
+            },
             findAddress: function () {
+                    var that = this;
                     var address = this.txtaddress.get('value');
 
                     var geocodeResultDiv = this.geocodeResultDiv;
-                    var templateObj = new dtl.Template (this._templateString);
+                    var templateObj = new dtl.Template(this._templateString);
 
 
 
-                    var requestURL = this._apiUrl + "?address=" + address + "&key=" + this._apiKey;
+                    var requestURL = this.config.apiUrl + "?address=" + address + "&key=" + this.config.apiKey;
                     request(requestURL, {
                         headers: {
                             "X-Requested-With": null
                         },
                         handleAs: "json"
                     }).then(function (data) {
+                        console.log(that);
                         console.log(data);
 
+                        var resultdata = new Object();
+                        resultdata.address = data.results[0].formatted_address;
+                        resultdata.lon = data.results[0].geometry.location.lng;
+                        resultdata.lat = data.results[0].geometry.location.lat;
+                        resultdata.type = data.results[0].geometry.location_type;
+                        that.zoomTo(resultdata.lon,resultdata.lat);
                         geocodeResultDiv.innerHTML = ''; //removes the existing content
-                        var contextObj  = new context({ result: data });
+                        var contextObj = new context({
+                            result: resultdata,
+                            zoomTo: function (x, y) {
+                                console.log(x + ',' + y);
+                            }
+                        });
                         var compiledTemplate = templateObj.render(contextObj);
                         var node = domConst.toDom(compiledTemplate);
 
