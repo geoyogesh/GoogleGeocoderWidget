0e6d6158c214d3e831194530f07418be590b4ffd
geoyogesh <geoyogesh@gmail.com>
2015-06-10 01:08:40 -0400

    added geocode api calls

--- Widget.js
+++ Widget.js
@@ -1,60 +1,96 @@
-define(['dojo/_base/declare', 'jimu/BaseWidget'],
-function(declare, BaseWidget) {
-  //To create a widget, you need to derive from BaseWidget.
-  return declare([BaseWidget], {
+define(['dojo/_base/declare',
+        'dijit/_AttachMixin',
+    'dijit/_WidgetsInTemplateMixin',
+        'jimu/BaseWidget',
+        'dojo/request',
+       'dojo/text!./_templates/google-result.html',
+       'dojo/parser',
+       'dojo/dom-construct','dojox/dtl/_base', 'dojox/dtl/Context'
+       ],
+    function (declare, _AttachMixin, _WidgetsInTemplateMixin, BaseWidget, request, template,parser,domConst,dtl, context) {
+        //To create a widget, you need to derive from BaseWidget.
+        return declare([BaseWidget, _WidgetsInTemplateMixin], {
 
-    // Custom widget code goes here
+            // Custom widget code goes here
 
-    baseClass: 'google-geocoder-widget',
-    // this property is set by the framework when widget is loaded.
-    // name: 'GoogleGeocoderWidget',
-    // add additional properties here
+            baseClass: 'google-geocoder-widget',
+            // this property is set by the framework when widget is loaded.
+            // name: 'GoogleGeocoderWidget',
+            _apiUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
+            _apiKey: 'AIzaSyC33aCsvTd4vAHA2f2gigNoEkd0IsbUhzc',
+            _templateString: template,
+            //methods to communication with app container:
+            postCreate: function () {
+                this.inherited(arguments);
+                console.log('GoogleGeocoderWidget::postCreate');
+            },
 
-    //methods to communication with app container:
-    postCreate: function() {
-      this.inherited(arguments);
-      console.log('GoogleGeocoderWidget::postCreate');
-    }
+            startup: function () {
+                this.inherited(arguments);
+                console.log('GoogleGeocoderWidget::startup');
+            },
 
-    // startup: function() {
-    //   this.inherited(arguments);
-    //   console.log('GoogleGeocoderWidget::startup');
-    // },
+            onOpen: function () {
+                console.log('GoogleGeocoderWidget::onOpen');
+            },
 
-    // onOpen: function(){
-    //   console.log('GoogleGeocoderWidget::onOpen');
-    // },
+            onClose: function () {
+                console.log('GoogleGeocoderWidget::onClose');
+            },
 
-    // onClose: function(){
-    //   console.log('GoogleGeocoderWidget::onClose');
-    // },
+            // onMinimize: function(){
+            //   console.log('GoogleGeocoderWidget::onMinimize');
+            // },
 
-    // onMinimize: function(){
-    //   console.log('GoogleGeocoderWidget::onMinimize');
-    // },
+            // onMaximize: function(){
+            //   console.log('GoogleGeocoderWidget::onMaximize');
+            // },
 
-    // onMaximize: function(){
-    //   console.log('GoogleGeocoderWidget::onMaximize');
-    // },
+            // onSignIn: function(credential){
+            //   console.log('GoogleGeocoderWidget::onSignIn', credential);
+            // },
 
-    // onSignIn: function(credential){
-    //   console.log('GoogleGeocoderWidget::onSignIn', credential);
-    // },
+            // onSignOut: function(){
+            //   console.log('GoogleGeocoderWidget::onSignOut');
+            // }
 
-    // onSignOut: function(){
-    //   console.log('GoogleGeocoderWidget::onSignOut');
-    // }
+            // onPositionChange: function(){
+            //   console.log('GoogleGeocoderWidget::onPositionChange');
+            // },
 
-    // onPositionChange: function(){
-    //   console.log('GoogleGeocoderWidget::onPositionChange');
-    // },
+            // resize: function(){
+            //   console.log('GoogleGeocoderWidget::resize');
+            // }
+            findAddress: function () {
+                    var address = this.txtaddress.get('value');
 
-    // resize: function(){
-    //   console.log('GoogleGeocoderWidget::resize');
-    // }
+                    var geocodeResultDiv = this.geocodeResultDiv;
+                    var templateObj = new dtl.Template (this._templateString);
 
-//methods to communication between widgets:
 
-  });
 
-});
+                    var requestURL = this._apiUrl + "?address=" + address + "&key=" + this._apiKey;
+                    request(requestURL, {
+                        headers: {
+                            "X-Requested-With": null
+                        },
+                        handleAs: "json"
+                    }).then(function (data) {
+                        console.log(data);
+
+                        geocodeResultDiv.innerHTML = ''; //removes the existing content
+                        var contextObj  = new context({ result: data });
+                        var compiledTemplate = templateObj.render(contextObj);
+                        var node = domConst.toDom(compiledTemplate);
+
+                        domConst.place(node, geocodeResultDiv);
+                        parser.parse(geocodeResultDiv);
+                    }, function (err) {
+                        console.log(err.message);
+                    });
+                }
+                //methods to communication between widgets:
+
+        });
+
+    });
