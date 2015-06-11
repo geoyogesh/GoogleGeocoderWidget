59d596161dfa86111584a0ed10696c658ce6c44e
geoyogesh <geoyogesh@gmail.com>
2015-06-10 21:59:06 -0400

    dgrid usage fix

--- Widget.js
+++ Widget.js
@@ -4,10 +4,14 @@ define(['dojo/_base/declare',
         'jimu/BaseWidget',
         'dojo/request',
        'dojo/text!./_templates/google-result.html',
-       'dojo/parser','esri/geometry/Point','esri/SpatialReference',
+       'dojo/parser', 'esri/geometry/Point', 'esri/SpatialReference', "dgrid/OnDemandGrid",
+      "dgrid/Selection", "dojo/number", "dojo/_base/array", "dojo/store/Memory","dojo/dom-style",
        'dojo/dom-construct', 'dojox/dtl/_base', 'dojox/dtl/Context'
+
        ],
-    function (declare, _AttachMixin, _WidgetsInTemplateMixin, BaseWidget, request, template, parser, Point,SpatialReference, domConst, dtl, context) {
+    function (declare, _AttachMixin, _WidgetsInTemplateMixin, BaseWidget, request, template, parser, Point, SpatialReference, Grid,
+        Selection, dojoNum, array, Memory,domStyle,
+        domConst, dtl, context) {
         //To create a widget, you need to derive from BaseWidget.
         return declare([BaseWidget, _WidgetsInTemplateMixin], {
 
@@ -21,6 +25,20 @@ define(['dojo/_base/declare',
             postCreate: function () {
                 this.inherited(arguments);
                 console.log('GoogleGeocoderWidget::postCreate');
+                this.grid = new(declare([Grid, Selection]))({
+                    // use Infinity so that all data is available in the grid
+                    bufferRows: Infinity,
+                    columns: {
+                        "select": "Select",
+                        "type": {
+                            "label": "Type"
+                        },
+                        "address": {
+                            "label": "Address"
+                        }
+                    }
+                }, this.cusgrid);
+                this.grid.on(".field-select:click", this.selectState);
             },
 
             startup: function () {
@@ -61,8 +79,10 @@ define(['dojo/_base/declare',
             // }
             zoomTo: function (x, y) {
                 console.log(x + ',' + y);
-                var point = new Point([x,y],new SpatialReference({ wkid:4326 }));
-                this.map.centerAndZoom(point,12);
+                var point = new Point([x, y], new SpatialReference({
+                    wkid: 4326
+                }));
+                this.map.centerAndZoom(point, 12);
             },
             findAddress: function () {
                     var that = this;
@@ -83,12 +103,28 @@ define(['dojo/_base/declare',
                         console.log(that);
                         console.log(data);
 
+                        var ndata = array.map(data.results, function (feature) {
+                            return {
+                                "select": 'select',
+                                "type": feature.geometry.location_type,
+                                "address": feature.formatted_address
+                            };
+                        });
+                        var memStore = new Memory({
+                            data: ndata
+                        });
+                        console.log(ndata);
+                        that.grid.set("store", memStore);
+                        that.grid.startup();
+                        //parser.parse(that.grid);
+                        //that.grid.renderArray(ndata);
+                        /*
                         var resultdata = new Object();
                         resultdata.address = data.results[0].formatted_address;
                         resultdata.lon = data.results[0].geometry.location.lng;
                         resultdata.lat = data.results[0].geometry.location.lat;
                         resultdata.type = data.results[0].geometry.location_type;
-                        that.zoomTo(resultdata.lon,resultdata.lat);
+                        that.zoomTo(resultdata.lon, resultdata.lat);
                         geocodeResultDiv.innerHTML = ''; //removes the existing content
                         var contextObj = new context({
                             result: resultdata,
@@ -101,10 +137,14 @@ define(['dojo/_base/declare',
 
                         domConst.place(node, geocodeResultDiv);
                         parser.parse(geocodeResultDiv);
+                        */
                     }, function (err) {
                         console.log(err.message);
                     });
-                }
+                },
+            selectState:function(e){
+                console.log(e);
+            }
                 //methods to communication between widgets:
 
         });
